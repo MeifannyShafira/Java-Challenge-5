d0238d6f68f66661699d8c8757d89212a441b25f
Meifanny Azri Shafira <meifannyshafira@gmail.com>
2021-10-22 11:18:19 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+class Main {
+    public static void main(String[] args) {
+      Heroes hero = new Heroes();
+  
+      hero.setProfile("Meifanny Azri Shafira", 21, 163);
+      hero.profile();
+      
+  
+      // jalankan aktivitas disini...
+      hero.jalan();
+      hero.jalan();
+      
+      hero.lari();
+      hero.lari();
+
+      hero.makan();
+      
+      hero.minum();
+      hero.minum();
+      hero.minum();
+      
+      hero.lompat();
+    
+      hero.duduk();
+      hero.duduk();
+     
+
+      // tampilkan status terkini...
+      hero.profile();
+      
+    }
+  }
